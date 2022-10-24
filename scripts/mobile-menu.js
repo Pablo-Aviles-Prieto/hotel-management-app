b9e016d15b98bfaba1e307b8379ae9148be5d849
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2022-10-24 21:52:44 +0200

    Header styled for both mobile and desktop

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,10 +1,10 @@
 const mobileBtn = document.querySelector('#mobile-menu-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
-const mobileMnuIcon = document.querySelector('.nav-navbar-mobile-menu-btn');
+const mobileMnuIcon = document.querySelector('.container-nav-navbar-mobile-menu-btn');
 
 const mobileMenuHandler = () => {
   mobileMenu.classList.toggle('mobile-menu--closed');
-  mobileMnuIcon.classList.toggle('nav-navbar-mobile-menu-btn--open');
+  mobileMnuIcon.classList.toggle('container-nav-navbar-mobile-menu-btn--open');
 };
 
 mobileBtn.addEventListener('click', mobileMenuHandler);
