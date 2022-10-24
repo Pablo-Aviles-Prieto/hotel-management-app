14b97f2310e50d12bb756a04c776c0e27d3f0da8
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2022-10-24 15:52:52 +0200

    Initial commit

---
+++ scripts/mobile-menu.js
@@ -0,0 +1,7 @@
+const mobileBtn = document.querySelector('#mobile-menu-btn');
+
+const mobileMenuHandler = () => {
+  console.log('test');
+};
+
+mobileBtn.addEventListener('click', mobileMenuHandler);
