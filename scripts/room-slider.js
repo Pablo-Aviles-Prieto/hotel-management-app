1808db2118fe916d27f6cbd4f48e29bdd5f19fb0
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2022-10-25 22:58:20 +0200

    rooms slider working on mobile

---
+++ scripts/room-slider.js
@@ -0,0 +1,15 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
+
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
