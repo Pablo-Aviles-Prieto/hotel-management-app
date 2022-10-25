7f2c71693def72c2677b8b32f587ac181bdabc3e
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2022-10-26 01:37:33 +0200

    Slider for the rooms working for desktop also

--- scripts/room-slider.js
+++ scripts/room-slider.js
@@ -1,9 +1,19 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
 const swiper = new Swiper('.mySwiper', {
-  slidesPerView: 1,
+  slidesPerView: 2,
   spaceBetween: 30,
   loop: true,
+  breakpoints: {
+    1300: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
