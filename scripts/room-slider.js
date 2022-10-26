5ea3bbcddd92b6d2094ff2130024dc7784702f96
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2022-10-26 17:11:30 +0200

    Added first card for the facilities slider

--- scripts/room-slider.js
+++ scripts/room-slider.js
@@ -1,6 +1,30 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
-const swiper = new Swiper('.mySwiper', {
+const swiperForRooms = new Swiper('.mySwiper', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    1300: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperForFacilities = new Swiper('.mySwiperFacilities', {
   slidesPerView: 2,
   spaceBetween: 30,
   loop: true,
