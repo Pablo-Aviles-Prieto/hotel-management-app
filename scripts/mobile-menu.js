fef437ce1847044a228b9c6847b267b4c3fcbf53
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2022-10-28 10:16:43 +0200

    introduction section of the about us page

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,10 +1,13 @@
 const mobileBtn = document.querySelector('#mobile-menu-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
-const mobileMnuIcon = document.querySelector('.container-nav-navbar-mobile-menu-btn');
+const mobileMnuIcon = document.querySelector(
+  '.container-nav-navbar-mobile-menu-btn'
+);
 
 const mobileMenuHandler = () => {
   mobileMenu.classList.toggle('mobile-menu--closed');
   mobileMnuIcon.classList.toggle('container-nav-navbar-mobile-menu-btn--open');
+  console.log('check');
 };
 
 mobileBtn.addEventListener('click', mobileMenuHandler);
